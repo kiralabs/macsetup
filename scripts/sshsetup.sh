1c4fb11d4c61142ce58e4d2ecff90da8e265d1d7
Izzy <me@kuugan.com>
2022-06-11 21:50:45 +0930

    Scripts: Add note to SSH key import

--- scripts/sshsetup.sh
+++ scripts/sshsetup.sh
@@ -14,16 +14,19 @@ white=`tput setaf 7`
 reset=`tput sgr0`
 
 # ask user for ssh keys
+mkdir -p ~/.ssh/
 echo "${yellow}[Status] Copying SSH keys"
-read -p 'Drag and Drop your Private SSH Key here: ' privlocation
-read -p 'Drag and Drop your Public SSH Key here: ' publocation
-cp $privlocation ~/.ssh/key
-cp $publocation ~/.ssh/key.pub
+echo "The directory you are copying your SSH keys from must not have any 
+spaces in them. This may be fixed in the future."
+read -p 'Drag and Drop your Private SSH Key here: ' privatekey
+read -p 'Drag and Drop your Public SSH Key here: ' publickey
+cp $privatekey ~/.ssh/key
+cp $publickey ~/.ssh/key.pub
 echo "Copying SSH Keys complete!${reset}"
 
 # set ssh key variables
-publickey=~/.ssh/key.pub
-privatekey=~/.ssh/key
+setpublickey=~/.ssh/key.pub
+setprivatekey=~/.ssh/key
 
 # start ssh agent
 echo "${cyan}[setup] Starting SSH agent"
@@ -39,7 +42,8 @@ echo "Host *
 # add ssh key to ssh-agent
 echo "[setup] Adding SSH key to ssh-agent"
 echo "${pink}[note] Using --apple-use-keychain only works in macOS 12.0 and later, assuming you are on this version.${reset}"
-ssh-add --apple-use-keychain $privatekey
+ssh-add --apple-use-keychain $setprivatekey
 
 # done
-echo "${green}SSH Key setup complete!${reset}"
+echo "${green}SSH Key setup complete! - Re-open your terminal before 
+continuing!${reset}"
